f61a274a14c335ff1c86f5f5af6ca1328213f824
junecodes767 <junecodes767@gmail.com>
2025-01-04 13:42:51 -0400

    Working version of the code

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from PIL import Image
 import os
 
-def resize_imh (img,new_width):
+def resize_img (img,new_width):
+   
    width,height =img.size
    ratio =height/width
    new_height = int(ratio*width)
@@ -9,4 +10,23 @@ def resize_imh (img,new_width):
    return resized_image 
 
 def main():
-   folder =os.listdir()
+   
+   img = Image.open("angry_cat-photo.jpg")
+   
+   mod_photo = resize_img(img,200) #resizes the photo 
+     
+   output_folder =os.listdir("Modified-photos") # turns the directory into a list
+   output_folder_path = "Modified-photos"
+   #place the image into the directory
+   output_path = os.path.join(output_folder_path, "angry_cat_resized.jpg")
+   #saves the image into the directory
+   mod_photo.save(output_path)
+   output_folder.append(mod_photo)    
+   print(output_folder)             
+   for photo in output_folder:
+       photo.show()
+       
+       
+if __name__ == "__main__":
+    main()
+    
