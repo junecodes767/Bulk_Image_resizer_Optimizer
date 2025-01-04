c1612487e23132627eb60c231607b0aefcf5e730
junecodes767 <junecodes767@gmail.com>
2025-01-04 13:57:58 -0400

    saving the path

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from PIL import Image
 import os
 
-def resize_imh (img,new_width):
+def resize_img (img,new_width):
+   img = Image.open(img)
    width,height =img.size
    ratio =height/width
    new_height = int(ratio*width)
@@ -9,4 +10,13 @@ def resize_imh (img,new_width):
    return resized_image 
 
 def main():
-   folder =os.listdir()
+   folder =os.listdir("Photos-to-adjust")#creates a list of the image files
+   folder_path ="Modified-photos"
+   # loop through the images
+   for photo in folder:
+      #resizes the image to desireable size
+      modified_image =resize_img(photo)
+      #add the photo to the new folder
+      next_location = os.path.join(folder_path,photo)
+      #save the path
+      
